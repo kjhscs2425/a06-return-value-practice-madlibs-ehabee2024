022e1515d9f258247a463502fcda07bf284dbcfe
ehabee <ebender2025@kehillahstudent.org>
2024-11-18 08:33:20 -0800

    done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun: ")
-
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+pluralnoun0 = input("Give me a plural noun: ")
+personinroomfemale0 = input("Give me a person in room: ")
+adjective0 = input("Give me a adjective")
+story = f "There are many {adjective0} ways to choose a/n {noun0} to read. First, you could ask for recommendations from your friends and {pluralnoun0}. Just dont ask Aunt {personinroomfemale0}-she only reads {adjective0} books with {articleofclothing0}-ripping goddesses on the cover. If your friends and family are no help, try checking out {noun0} Review in The {acity0} Times. If the {pluralnoun0} featured there are too {adjective0} for your taste, try something a little
 print(story)
